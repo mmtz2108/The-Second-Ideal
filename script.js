17ad2106ded19b259ac3a9ad2d1c0779e36e9878
Marcos Martinez <mmtz2108@gmail.com>
2022-03-27 18:00:05 -0500

    added a simple click function and some new text

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "I am struggling with JavaScript but learning as I go. Bear with me.";
 document.querySelector("body").appendChild(h2);
+
+const Marcos = document.getElementById('Marcos');
+Marcos.addEventListener('click', function() {
+  alert(`How often do you click on stranger's faces?`);
+});
